c5e15baa8dd801af433845fbf72fec3833798a9c
Crowdbotics <team@crowdbotics.com>
2023-03-26 22:24:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-morning-39557",
+  url: "https://yellow-morning-39557.botics.co",
+  api: "https://yellow-morning-39557.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
